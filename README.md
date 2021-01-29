d15feb1c35370de30677d1f1d4cc05daa2600db9
Milly Schmidt <meeli.jane@gmail.com>
2021-01-29 19:49:20 +1100

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-## Milly's personal website
-
-Hello, thank you for stopping by. Visit the website [here](https://www.millyschmidt.me).
--- gatsby-browser.js
+++
@@ -1,5 +0,0 @@
-import "./src/styles/global.css";
-import "@fontsource/libre-franklin"
-import "@fontsource/libre-franklin/400-italic.css"
-import "@fontsource/libre-franklin/800.css"
-import "@fontsource/libre-franklin/600.css"
